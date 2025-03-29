43658cd2d522cea525e045abe9edf1fc1146777b
Melissa Luc <melissa.ml.luc@gmail.com>
2025-03-28 20:51:19 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -11,11 +11,11 @@ export type Metadata = {
   DESCRIPTION: string;
 };
 
+export type IconName = "instagram" | "github" | "linkedin"
+
 export type Socials = {
   NAME: string;
   HREF: string;
-  ICON: string;
+  ICON: IconName;
 }[];
 
-
-export type IconName = "instagram" | "github" | "linkedin"
